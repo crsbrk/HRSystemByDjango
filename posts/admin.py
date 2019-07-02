7ad0c0a7e2dab11fd269704fc4ac46962e34efa7
crsbrk <chris.zhch@gmail.com>
2019-07-02 10:01:50 +0800

    add a worker filter3

--- posts/admin.py
+++ posts/admin.py
@@ -5,7 +5,7 @@ from django.contrib import admin
 from .models import Posts
 
 class PostsAdmin(admin.ModelAdmin):
-    list_display = ('title','pj_score','is_not_delayed','pj_leader','workload_allot','pj_participant1','workload_allot1','pj_participant2','workload_allot2','pj_participant3','workload_allot3')
+    list_display = ('title','pj_score','deadline_at','is_not_delayed','pj_leader','workload_allot','pj_participant1','workload_allot1','pj_participant2','workload_allot2','pj_participant3','workload_allot3')
     list_per_page = 25
     search_fields = ('title','pj_leader','pj_participant1','pj_participant2','pj_participant3','deadline_at',)
     list_filter = ('pj_score','is_not_delayed',)
