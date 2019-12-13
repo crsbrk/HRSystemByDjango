e18d9c1c656d21727af323f3f4a87710e0cf801a
crsbrk <chris.zhch@gmail.com>
2019-12-13 14:33:28 +0800

    add pregress bar

--- faulty/admin.py
+++ faulty/admin.py
@@ -6,12 +6,12 @@ from .models import Faulty
 
 
 class FaultyAdmin(admin.ModelAdmin):
-    list_display = ('title', 'pj_score','pj_type', 'is_not_delayed', 'pj_leader', 'workload_allot', 'pj_participant1',
+    list_display = ('title', 'pj_score','pj_type', 'is_not_delayed', 'created_at','pj_leader', 'workload_allot', 'pj_participant1',
                     'workload_allot1', 'pj_participant2', 'workload_allot2', 'pj_participant3', 'workload_allot3')
     list_per_page = 25
     list_filter = ('pj_type','created_at')
     search_fields = ('title', 'pj_score', 'pj_leader','pj_type',
-                     'pj_participant1', 'pj_participant2', 'pj_participant3','created_at','created_at',)
+                     'pj_participant1', 'pj_participant2', 'pj_participant3','created_at',)
 
 
 admin.site.register(Faulty, FaultyAdmin)
