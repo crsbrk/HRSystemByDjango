da3ae0bc09e0fbebb548b9d59bf783269d08b200
crsbrk <chris.zhch@gmail.com>
2020-12-18 17:37:34 +0800

    2020season4

--- scores/views.py
+++ scores/views.py
@@ -123,9 +123,9 @@ def index(request):
         i += 1
 
 
-    updateScoreOfWorkers(2020, 7)
+    updateScoreOfWorkers(2020, 9)
     # print(scoreOfAllWorkers)
-    updateScoreOfWorkers(2020, 8)
+    updateScoreOfWorkers(2020, 9)
     # print(scoreOfAllWorkers)
     updateScoreOfWorkers(2020, 9)
     # print(scoreOfAllWorkers)
@@ -298,7 +298,7 @@ def getJixiaoByItemsLimit():
     season4 = Scores.objects.raw('''SELECT worker_name as id,sum(score_posts) postScores,sum(score_orders) orderScores,
 	sum(score_cutovers) cutoverScores, sum(score_bonuses) bonuseScores, sum(score_faulty) faultyScores, sum(score_routine) routineScores  
     from scores_scores
-    where score_year_month in('2020-7','2020-8','2020-9') and  
+    where score_year_month in('2020-10','2020-11','2020-12') and  
     worker_name in('张晨','常晓波','陈立栋','韦国锐','黄锵栩','汪志武','苏飓','霍晓歌','李晓昕','郭少钏','于秋思','苏伟衡','杨晓','刘峰','刘江','刘雷')
     GROUP BY worker_name
 ''')
